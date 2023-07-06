8c97ab7c56ccd331264e34afdf7492b5f348d94e
jeanne islande <jeanneislandez@gmail.com>
2023-07-06 14:07:32 +0000

    ajout des fonction multiplication et division et les tests

---
+++ fonctions.py
@@ -0,0 +1,10 @@
+# Fonction de multiplication 
+def multiplication(a, b):
+    return a * b
+
+# Fonction de division
+def division(a, b):
+    if b != 0:
+        return a / b
+    else:
+        return "Erreur : division par zéro"
--- test.py
+++ test.py
@@ -1,1 +1,84 @@
 from fonctions import *
+import math
+def test_multiplication():
+    # Test avec des nombres positifs
+    input_num1 = 2
+    input_num2 = 3
+    expected_output = 6
+    try:
+        assert multiplication(input_num1,input_num2) == expected_output
+        print("Test case 2 passed.")
+    except AssertionError:
+        print("Test case 2 failed.")
+        
+        
+        # Test avec des nombres null
+    input_num1 = 2
+    input_num2 = 0
+    expected_output = 0
+    try:
+        assert multiplication(input_num1,input_num2) == expected_output
+        print("Test case 2 passed.")
+    except AssertionError:
+        print("Test case 2 failed.")
+        
+        # Test avec des nombres negatif
+    input_num1 = 2
+    input_num2 = -3
+    expected_output = -6
+    try:
+        assert multiplication(input_num1,input_num2) == expected_output
+        print("Test case 2 passed.")
+    except AssertionError:
+        print("Test case 2 failed.")
+        
+def test_division():
+            
+         # Test avec 1
+    input_num1 = 10
+    input_num2 = 1
+    expected_output = 10
+    try:
+        assert division(input_num1,input_num2) == expected_output
+        print("Test case 2 passed.")
+    except AssertionError:
+        print("Test case 2 failed.")
+        
+        
+         # Test avec des nombres positifs
+    input_num1 = 18
+    input_num2 = 2
+    expected_output = 9
+    try:
+        assert division(input_num1,input_num2) == expected_output
+        print("Test case 2 passed.")
+    except AssertionError:
+        print("Test case 2 failed.")
+        
+         # Test avec des nombres positifs
+    input_num1 = 6
+    input_num2 = -3
+    expected_output = 2
+    try:
+        assert division(input_num1,input_num2) == expected_output
+        print("Test case 2 passed.")
+    except AssertionError:
+        print("Test case 2 failed.")
+        
+         # Test avec le nombre null 
+    input_num1 = 15
+    input_num2 = 0
+    expected_output = "Erreur : Division par zéro"
+    try:
+        assert division(input_num1,input_num2) == expected_output
+        print("Test case 2 passed.")
+    except AssertionError:
+        print("Test case 2 failed.")
+        
+        
+       
+       # Appel des fonctions de test
+test_multiplication()
+test_division() 
+        
+    
